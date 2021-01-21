703e6f139522896a09b3a026d8d928c6cffca03a
A.J. Romaniello <ajiellodev@gmail.com>
2021-01-21 00:11:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function findAllPermutations(str) {
   let arr = []
   for (let i = 0; i < str.length; i++) {
     let e = str[i];
-    console.log(str[i])
+    console.log(str[i], str.substring(0, i) + str.substring(i + 1, str.length))
   }
 
   return arr;
