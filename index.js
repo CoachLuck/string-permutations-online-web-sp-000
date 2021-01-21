b0680b035bbb19dc1af02a6da4585500a9301188
A.J. Romaniello <ajiellodev@gmail.com>
2021-01-21 00:04:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-function findAllPermutations() {
-
+function findAllPermutations(str) {
+  let arr = [str]
+  for (let i = 1; i < str.length; i++) {
+    
+  }
 }
 
 console.log(findAllPermutations("abc"))
