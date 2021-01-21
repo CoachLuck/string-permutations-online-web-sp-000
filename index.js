daaa50f19ef44b1015c245b2ef84f6659287c128
A.J. Romaniello <ajiellodev@gmail.com>
2021-01-21 00:24:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ function find(str, start, end) {
   }
   for (let i = start; i < end; i++) {
     str = swap(str, start, i)
+    console.log(str)
     find(str, start + 1, end)
     str = swap(str, start, i)
   }
